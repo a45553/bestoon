fe4bc9bf2ec64a526c62687c8736de6238f9d897
Zangeneh <zangeneh@nli.org>
2020-08-24 12:52:16 +0430

    models make...

--- web/models.py
+++ web/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import jdatetime
+
 
 # Create your models here.
 class Expense(models.Model):
@@ -7,4 +9,11 @@ class Expense(models.Model):
     date=models.DateTimeField()
     amount=models.BigIntegerField()
     user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} - {}".format(jdatetime.date(self.date()).strftime("%d - %m - %Y"),self.amount)
     
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
